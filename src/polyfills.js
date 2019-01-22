6a7f503cdc029ce821443536c420ced4b7dc4465
bokotomo <bokotomo@me.com>
2019-01-22 18:52:55 +0900

    fix: Overwritesをコメントアウト

--- src/polyfills.js
+++ src/polyfills.js
@@ -15,6 +15,7 @@
   }
 })(global.Node || global.Element);
 
+/*
 // Overwrites native 'firstElementChild' prototype.
 // Adds Document & DocumentFragment support for IE9 & Safari.
 // Returns array instead of HTMLCollection.
@@ -39,6 +40,7 @@
     });
   }
 })(global.Node || global.Element);
+*/
 
 // Production steps of ECMA-262, Edition 5, 15.4.4.17
 // Reference: http://es5.github.io/#x15.4.4.17
